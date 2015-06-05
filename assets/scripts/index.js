766be9b047e6796168c5154211aa1346d89f479e
Qianglong Mo <sammok2003@hotmail.com>
2015-06-05 17:11:53 +0800

    Init repos.

---
+++ assets/scripts/index.js
@@ -0,0 +1,16 @@
+// Created by sam mok (Siso brand interactive team.) 2015
+
+var index = {
+    components: {
+
+    },
+
+    initUi: function (){
+
+    }
+};
+
+$(function (){
+    // init index 
+    index.initUi();
+});
---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+// Created by sam mok (Siso brand interactive team.) 2015
+
+"use strict";
+
+var app = {
+    components: {
+    
+    },
+    initApp: function (){
+    
+    }
+};
+
+$(function (){
+    // init app
+    app.initApp();
+});
